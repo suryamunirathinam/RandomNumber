cf48fbcdf7dd21673e507021d9e499f76aa6da8b
Surya Munirathinam <suryamunirathinam@gmail.com>
2020-07-06 01:01:40 +0530

    updated remining time

--- RandomNumber/rateLimitingService/views.py
+++ RandomNumber/rateLimitingService/views.py
@@ -1,4 +1,4 @@
-import datetime
+from datetime import datetime,timedelta
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
@@ -26,30 +26,35 @@ def signup(request):
 def generate_rand_no(request):
     rand_no = random.randint(1, 9999)
     count = get_usage(request, group=None,fn=generate_rand_no, key ='post:username', method='GET', rate = '5/m',increment=True)
-    request.session['limit']=5-count['count']
+    request.session['limit'] = 5-count['count']
+    request.session['hits'] = count['count']
     if request.session['limit'] == 4:
-        request.session['time'] = datetime.datetime.now().__str__()
+        time = datetime.now()
+        request.session['time'] = time.__str__()
+        request.session['after']= (time +timedelta(hours=1)).__str__()
     return render(request, 'random_no.html', {
         'randomnumber': rand_no,
+        'remaining':count
     })
 
 @login_required
 def remainingLimits(request):
     try:
         datetimeFormat = '%Y-%m-%d %H:%M:%S.%f'
-        date1 = request.session['time']
-        date2 = datetime.datetime.now().__str__()
-        diff = datetime.datetime.strptime(date2, datetimeFormat) \
-               - datetime.datetime.strptime(date1, datetimeFormat)
-        print (diff.seconds)
-        if diff.seconds > 60:
+        date1 = datetime.now().__str__()
+        print(date1)
+        date2 = request.session['after']
+        print(date2)
+        diff = datetime.strptime(date2, datetimeFormat) \
+               - datetime.strptime(date1, datetimeFormat)
+        print(diff.seconds)
+        limit = int(diff.seconds/60)*5 - request.session['hits']
+        print(limit)
 
-            limit = 5
-        else:
-            limit = request.session['limit']
+
     except Exception as e:
         print(e)
-        limit = 5
+        limit = 300
 
     return render(request, 'remaining_limits.html', {
         'Remaininglimit':limit
